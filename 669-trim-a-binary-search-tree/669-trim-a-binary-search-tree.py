ef1a0baf301ab0d92ade303b8b1d87508f8b4281
Max Goodwin <maxgoodwin3@pm.me>
2022-04-15 16:58:36 +1000

    Time: 58 ms (73.14%), Space: 16.2 MB (94.15%) - LeetHub

---
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+    if not root: return None
+    
+    root.left = self.trimBST(root.left, low, high)
+    root.right = self.trimBST(root.right, low, high)
+    
+    if root.val < low:
+      root = self.trimBST(root.right, low, high)
+    
+    if root and root.val > high:
+      root = self.trimBST(root.left, low, high)
+    
+    return root
