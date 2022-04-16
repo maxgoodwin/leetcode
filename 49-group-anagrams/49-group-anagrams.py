e5c81d99db520ce6d400bb915249863e3b8c85c2
Max Goodwin <maxgoodwin3@pm.me>
2022-04-16 19:18:16 +1000

    Time: 122 ms (69.37%), Space: 19.9 MB (18.09%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,23 +1,13 @@
 class Solution:
   def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    sCharDict: dict[frozenset, list[str]] = dict()
+    anagrams = collections.defaultdict(list)
     
     for s in strs:
-      sSet: set[str] = set()
-      sDict: dict[str, int] = dict()
+      charCount = [0] * 26
       
       for char in s:
-        if char not in sDict: sDict[char] = 0
-          
-        sDict[char] += 1
-        sSet.add(char + str(sDict[char]))
-      
-      frozenSSet = frozenset(sSet)
+        charCount[ord(char) - ord('a')] += 1
         
-      if frozenSSet not in sCharDict: sCharDict[frozenSSet] = [s]
-      else: 
-        lst = sCharDict[frozenSSet]
-        lst.append(s)
-        sCharDict[frozenSSet] = lst
-        
-    return sCharDict.values()
+      anagrams[tuple(charCount)].append(s)
+    
+    return anagrams.values()
