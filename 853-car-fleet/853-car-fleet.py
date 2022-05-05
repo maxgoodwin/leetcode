7f32adf3c773993e2669d609056f9b3ffd571d67
Max Goodwin <maxgoodwin3@pm.me>
2022-05-05 16:51:02 +1000

    Time: 1301 ms (44.38%), Space: 36.5 MB (41.56%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,19 +1,11 @@
 class Solution:
   def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
     fleetStack = deque()
-    positionSpeed = [0] * (max(position) + 1)
+    positionSpeed = [(pos, speed) for pos, speed in zip(position, speed)]
+    positionSpeed.sort(reverse=True)
     
-    for i in range(len(position)):
-      positionSpeed[position[i]] = speed[i]
-    
-    fleetStack.append((len(positionSpeed) - 1, positionSpeed[-1]))
-    
-    for pos in range(len(positionSpeed) -2, -1, -1):
-      if positionSpeed[pos] == 0: continue
-      
-      if (target - pos)/positionSpeed[pos] <= (target - fleetStack[-1][0])/fleetStack[-1][1]:
-        continue
-        
-      fleetStack.append((pos, positionSpeed[pos]))
+    for posSpeed in positionSpeed:
+      if not fleetStack or (target - posSpeed[0])/posSpeed[1] > (target - fleetStack[-1][0])/fleetStack[-1][1]:
+        fleetStack.append(posSpeed)
     
     return len(fleetStack)
