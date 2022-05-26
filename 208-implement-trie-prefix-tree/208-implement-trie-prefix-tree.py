9ae844e1f71873621938b66c4aee496929527fd9
Max Goodwin <maxgoodwin3@pm.me>
2022-05-27 00:26:45 +1000

    Time: 154 ms (92.30%), Space: 27.6 MB (87.79%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,61 +1,27 @@
 class Trie:
   def __init__(self):
-    self.children = [None] * 26
+    self.trie = {}
   
-  def insert(self, word: str) -> None:
-    currentChildren = self.children
-    
-    for i in range(len(word) - 1):
-      alphabetPos = ord(word[i]) - 97
-      
-      if currentChildren[alphabetPos] == None:
-        currentChildren[alphabetPos] = Node()
-      
-      currentChildren = currentChildren[alphabetPos].children
-    
-    lastCharPos = ord(word[-1]) - 97
-    
-    if currentChildren[lastCharPos] == None:
-      currentChildren[lastCharPos] = Node()
-      
-    currentChildren[lastCharPos].wordEnd = True
-
-  def search(self, word: str) -> bool:
-    currentChildren = self.children
-    
-    for i in range(len(word) - 1):
-      alphabetPos = ord(word[i]) - 97
-      if currentChildren[alphabetPos] == None:
-        return False
-      
-      currentChildren = currentChildren[alphabetPos].children
-    
-    lastCharPos = ord(word[-1]) - 97
-    
-    if currentChildren[lastCharPos] == None or not currentChildren[lastCharPos].wordEnd:
-      return False
-    
+  def insert(self, word):
+    t = self.trie
+    for c in word:
+      if c not in t: t[c] = {}
+      t = t[c]
+    t['wordEnd'] = True
+  
+  def search(self, word):
+    t = self.trie
+    for c in word:
+      if c not in t: return False
+      t = t[c]
+    return 'wordEnd' in t
+  
+  def startsWith(self, prefix):
+    t = self.trie
+    for c in prefix:
+      if c not in t: return False
+      t = t[c]
     return True
-
-  def startsWith(self, prefix: str) -> bool:
-    currentChildren = self.children
-    
-    for char in prefix:
-      alphabetPos = ord(char) - 97
-      
-      if currentChildren[alphabetPos] == None:
-        return False
-      
-      currentChildren = currentChildren[alphabetPos].children
-    
-    return True
-      
-class Node:
-  def __init__(self):
-    self.val = True
-    self.wordEnd = False
-    self.children = [None] * 26
-      
       
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
